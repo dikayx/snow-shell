bafe6f5c4bdcf5b7959ece3ad4f7efcb947ccd0a
Dan Koller <57103678+dikayx@users.noreply.github.com>
2024-12-27 17:39:05 +0100

    Added a Christmas tree

--- snow.sh
+++ snow.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # Default configurations
-SPEED=14
+SPEED=4
 PARTICLE="*"
 COLUMNS=$(tput cols)
 ROWS=$(tput lines)
@@ -37,6 +37,45 @@ print_character() {
     esac
 }
 
+# Function to draw the Christmas tree
+draw_christmas_tree() {
+    local tree_base_row=$((ROWS - 10))
+    local tree_base_col=$((COLUMNS / 2))
+    local tree_height=8
+
+    # Draw the tree layers
+    for i in {0..7}; do
+        local start_col=$((tree_base_col - i))
+        local end_col=$((tree_base_col + i))
+        for j in $(seq $start_col $end_col); do
+            print_character $((tree_base_row + i)) $j "^" "green"
+        done
+    done
+
+    # Draw the trunk
+    local trunk_start=$((tree_base_col - 1))
+    local trunk_end=$((tree_base_col + 1))
+    for i in {0..1}; do
+        for j in $(seq $trunk_start $trunk_end); do
+            print_character $((tree_base_row + tree_height + i)) $j "|" "yellow"
+        done
+    done
+
+    # Add the star on top
+    print_character $((tree_base_row - 1)) $tree_base_col "*" "yellow"
+
+    # Add blinking ornaments (random colored balls) within the tree bounds
+    for _ in {1..15}; do
+        local layer=$((RANDOM % tree_height)) # Random tree layer (0 to tree_height - 1)
+        local start_col=$((tree_base_col - layer))
+        local end_col=$((tree_base_col + layer))
+        local ornament_col=$((start_col + RANDOM % (end_col - start_col + 1)))
+        local ornament_row=$((tree_base_row + layer))
+        local color=$(get_random_color)
+        print_character $ornament_row $ornament_col "o" $color
+    done
+}
+
 # Function to create snowfall
 snowfall() {
     clear_screen
@@ -45,6 +84,9 @@ snowfall() {
     declare -A snowflakes
 
     while true; do
+        # Draw the Christmas tree
+        draw_christmas_tree
+
         # Randomly add new snowflakes
         col=$((RANDOM % (COLUMNS - 1) + 1))
 
@@ -77,5 +119,6 @@ snowfall() {
 # Run snowfall
 snowfall
 
-# TODO: Color change doesn't work right now. Fix it. (example: ./snow.sh --speed 20 --particle "*" --color "cyan")
-# TODO: Add a chistmas tree at the bottom center of the screen. Should be green with random colored balls that blink and a star on top.
+# TODO: Add a bottom layer of snow on the ground
+# TODO: Make the snowflakes fall slower than the tree ornaments blink
+# TODO: Hide snowflakes if they are behind the tree
